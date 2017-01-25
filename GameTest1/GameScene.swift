b6b6b5294e302c876bf3b65451f8c208d7077ff5
Parker Timmerman <parker.timmerman@uconn.edu>
2017-01-25 11:17:44 -0500

    Initial Commit
    Swift 3 project playing with and learning 2D game development for iOS
    using SpriteKit

--- GameTest1/GameScene.swift
+++ GameTest1/GameScene.swift
@@ -9,81 +9,259 @@
 import SpriteKit
 import GameplayKit
 
-class GameScene: SKScene {
+struct PhysicsCategory {
+    static let ghost : UInt32 = 0x1 << 1
+    static let ground : UInt32 = 0x1 << 2
+    static let wall : UInt32 = 0x1 << 3
+    static let score : UInt32 = 0x1 << 4
+}
+
+class GameScene: SKScene, SKPhysicsContactDelegate {
     
-    private var label : SKLabelNode?
-    private var spinnyNode : SKShapeNode?
+    var _ground = SKSpriteNode()
+    var _ghost = SKSpriteNode()
+    var _wallPair = SKNode()
     
-    override func didMove(to view: SKView) {
+    var moveAndRemove = SKAction()
+    var gameStarted = Bool()
+    
+    var score = Int()
+    let scoreLabel = SKLabelNode()
+    
+    var died = Bool()
+    
+    var restartButton = SKSpriteNode()
+    
+    func restartScene()
+    {
+        self.removeAllChildren()
+        self.removeAllActions()
+        died = false
+        gameStarted = false
+        score = 0
+        createScene()
+    }
+    
+    func createScene()
+    {
+        self.physicsWorld.contactDelegate = self                    //says we will handle physics contacts
         
-        // Get label node from scene and store it for use later
-        self.label = self.childNode(withName: "//helloLabel") as? SKLabelNode
-        if let label = self.label {
-            label.alpha = 0.0
-            label.run(SKAction.fadeIn(withDuration: 2.0))
+        for i in 0..<2
+        {
+            let background = SKSpriteNode(imageNamed: "Background")
+            background.anchorPoint = CGPoint(x: 0, y: 0)
+            background.position = CGPoint(x: CGFloat(i-1) * self.frame.width, y: -self.frame.width)
+            background.size = (self.view?.bounds.size)!
+            background.name = "background"
+            self.addChild(background)
         }
         
-        // Create shape node to use during mouse interaction
-        let w = (self.size.width + self.size.height) * 0.05
-        self.spinnyNode = SKShapeNode.init(rectOf: CGSize.init(width: w, height: w), cornerRadius: w * 0.3)
+        scoreLabel.position = CGPoint(x: 0, y: 150)
+        scoreLabel.text = "\(score)"
+        scoreLabel.fontColor = UIColor.black
+        scoreLabel.fontSize = 60
+        scoreLabel.fontName = "04b_19"
         
-        if let spinnyNode = self.spinnyNode {
-            spinnyNode.lineWidth = 2.5
+        scoreLabel.zPosition = 5
+        
+        _ground = SKSpriteNode(imageNamed: "Ground")                //associating an image with the sprite
+        _ground.setScale(0.5)
+        
+        _ground.position = CGPoint(x: 0, y: (-(scene?.size.height)! / 2) + _ground.size.height / 2)   //set position of the ground's center to be middle of screen
+        _ground.physicsBody = SKPhysicsBody(rectangleOf: _ground.size)
+        _ground.physicsBody?.categoryBitMask = PhysicsCategory.ground
+        _ground.physicsBody?.collisionBitMask = PhysicsCategory.ghost
+        _ground.physicsBody?.contactTestBitMask = PhysicsCategory.ghost
+        _ground.physicsBody?.affectedByGravity = false
+        _ground.physicsBody?.isDynamic = false
+        
+        _ground.zPosition = 3
+        
+        self.addChild(_ground) //adding ground to the scene
+        
+        _ghost = SKSpriteNode(imageNamed: "Ghost")
+        _ghost.size = CGSize(width: 70, height: 70)
+        _ghost.position = CGPoint(x: -10, y: 0)
+        
+        _ghost.physicsBody = SKPhysicsBody(circleOfRadius: _ghost.frame.height / 2)
+        _ghost.physicsBody?.categoryBitMask = PhysicsCategory.ghost
+        _ghost.physicsBody?.collisionBitMask = PhysicsCategory.wall | PhysicsCategory.ground
+        _ghost.physicsBody?.contactTestBitMask = PhysicsCategory.wall | PhysicsCategory.ground | PhysicsCategory.score
+        _ghost.physicsBody?.affectedByGravity = true
+        _ghost.physicsBody?.isDynamic = true
+        
+        _ghost.zPosition = 2
+        
+        self.addChild(_ghost)
+        self.addChild(scoreLabel)
+    }
+    
+    override func didMove(to view: SKView)
+    {
+        createScene()
+    }
+    
+    func createButton()
+    {
+        restartButton = SKSpriteNode(imageNamed: "RestartBtn")
+        restartButton.size = CGSize(width: 200, height: 100)
+        restartButton.position = CGPoint(x: 0, y: 100)
+        restartButton.zPosition = 6
+        restartButton.setScale(0)
+        self.addChild(restartButton)
+        
+        restartButton.run(SKAction.scale(to: 1.0, duration: 0.2))
+    }
+    
+    func didBegin(_ contact: SKPhysicsContact)
+    {
+        let firstBody = contact.bodyA
+        let secondBody = contact.bodyB
+        
+        if firstBody.categoryBitMask == PhysicsCategory.score && secondBody.categoryBitMask == PhysicsCategory.ghost || firstBody.categoryBitMask == PhysicsCategory.ghost && secondBody.categoryBitMask == PhysicsCategory.score
+        {
+            score += 1
+            print(score)
+            scoreLabel.text = "\(score)"
+        }
+        
+        if firstBody.categoryBitMask == PhysicsCategory.ghost && secondBody.categoryBitMask == PhysicsCategory.wall || firstBody.categoryBitMask == PhysicsCategory.wall && secondBody.categoryBitMask == PhysicsCategory.ghost
+        {
+            enumerateChildNodes(withName: "wallPair", using: ({
+                (node, error) in
+                node.speed = 0
+                self.removeAllActions()
+            }))
             
-            spinnyNode.run(SKAction.repeatForever(SKAction.rotate(byAngle: CGFloat(M_PI), duration: 1)))
-            spinnyNode.run(SKAction.sequence([SKAction.wait(forDuration: 0.5),
-                                              SKAction.fadeOut(withDuration: 0.5),
-                                              SKAction.removeFromParent()]))
+            if died == false
+            {
+                died = true
+                createButton()
+            }
         }
     }
     
-    
-    func touchDown(atPoint pos : CGPoint) {
-        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
-            n.position = pos
-            n.strokeColor = SKColor.green
-            self.addChild(n)
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?)
+    {
+        if gameStarted == false
+        {
+            gameStarted = true
+            let spawn = SKAction.run({
+                () in
+                
+                self.createWalls()
+            })
+            
+            let delay = SKAction.wait(forDuration: 2.0)
+            let spawnDelay = SKAction.sequence([spawn, delay])
+            let spawnDelayForever = SKAction.repeatForever(spawnDelay)
+            self.run(spawnDelayForever)
+            
+            let distance = CGFloat(self.frame.width * 2 + _wallPair.frame.width)
+            let movePipes = SKAction.moveBy(x: -distance, y: 0, duration: TimeInterval(0.008 * distance))
+            let removePipes = SKAction.removeFromParent()
+            moveAndRemove = SKAction.sequence([movePipes, removePipes])
+            
+            _ghost.physicsBody?.velocity = CGVector(dx: 0, dy: 0)
+            _ghost.physicsBody?.applyImpulse(CGVector(dx: 0, dy: 90))
         }
-    }
-    
-    func touchMoved(toPoint pos : CGPoint) {
-        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
-            n.position = pos
-            n.strokeColor = SKColor.blue
-            self.addChild(n)
-        }
-    }
-    
-    func touchUp(atPoint pos : CGPoint) {
-        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
-            n.position = pos
-            n.strokeColor = SKColor.red
-            self.addChild(n)
-        }
-    }
-    
-    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
-        if let label = self.label {
-            label.run(SKAction.init(named: "Pulse")!, withKey: "fadeInOut")
+        else
+        {
+            if died == true
+            {
+                
+            }
+            else
+            {
+                _ghost.physicsBody?.velocity = CGVector(dx: 0, dy: 0)
+                _ghost.physicsBody?.applyImpulse(CGVector(dx: 0, dy: 90))
+            }
         }
         
-        for t in touches { self.touchDown(atPoint: t.location(in: self)) }
+        for touch in touches
+        {
+            let location = touch.location(in: self)
+            
+            if died == true
+            {
+                if restartButton.contains(location)
+                {
+                    restartScene()
+                }
+            }
+        }
     }
     
-    override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
-        for t in touches { self.touchMoved(toPoint: t.location(in: self)) }
-    }
     
-    override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
-        for t in touches { self.touchUp(atPoint: t.location(in: self)) }
-    }
-    
-    override func touchesCancelled(_ touches: Set<UITouch>, with event: UIEvent?) {
-        for t in touches { self.touchUp(atPoint: t.location(in: self)) }
-    }
-    
-    
-    override func update(_ currentTime: TimeInterval) {
+    override func update(_ currentTime: TimeInterval)
+    {
         // Called before each frame is rendered
+        if gameStarted == true{
+            if died == false
+            {
+                enumerateChildNodes(withName: "background", using: ({
+                    (node, error) in
+                    let bg = node as! SKSpriteNode
+                    bg.position = CGPoint(x: bg.position.x - 2, y: bg.position.y)
+                    
+                    if bg.position.x <= -bg.size.width
+                    {
+                        bg.position = CGPoint(x: bg.position.x + bg.size.width, y: bg.position.y)
+                    }
+                }))
+            }
+        }
+    }
+    
+    func createWalls()
+    {
+        let scoreNode = SKSpriteNode()
+        scoreNode.size = CGSize(width: 1, height: 200)
+        scoreNode.position = CGPoint(x: 500, y: 0)
+        scoreNode.physicsBody = SKPhysicsBody(rectangleOf: scoreNode.size)
+        scoreNode.physicsBody?.affectedByGravity = false;
+        scoreNode.physicsBody?.isDynamic = false;
+        scoreNode.physicsBody?.categoryBitMask = PhysicsCategory.score
+        scoreNode.physicsBody?.collisionBitMask = 0
+        scoreNode.physicsBody?.contactTestBitMask = PhysicsCategory.ghost
+        scoreNode.color = SKColor.blue
+      
+        _wallPair = SKNode()
+        _wallPair.name = "wallPair"
+        
+        let topWall = SKSpriteNode(imageNamed: "wall")
+        let bottomWall = SKSpriteNode(imageNamed: "wall")
+        
+        topWall.physicsBody = SKPhysicsBody(rectangleOf: topWall.size)
+        topWall.physicsBody?.categoryBitMask = PhysicsCategory.wall
+        topWall.physicsBody?.collisionBitMask = PhysicsCategory.ghost
+        topWall.physicsBody?.contactTestBitMask = PhysicsCategory.ghost
+        topWall.physicsBody?.affectedByGravity = false
+        topWall.physicsBody?.isDynamic = false
+        
+        bottomWall.physicsBody = SKPhysicsBody(rectangleOf: topWall.size)
+        bottomWall.physicsBody?.categoryBitMask = PhysicsCategory.wall
+        bottomWall.physicsBody?.collisionBitMask = PhysicsCategory.ghost
+        bottomWall.physicsBody?.contactTestBitMask = PhysicsCategory.ghost
+        bottomWall.physicsBody?.affectedByGravity = false
+        bottomWall.physicsBody?.isDynamic = false
+        
+        topWall.position = CGPoint(x: 500, y: 400)
+        bottomWall.position = CGPoint(x: 500, y: -400)
+        
+        topWall.setScale(0.5)
+        bottomWall.setScale(0.5)
+        
+        topWall.zRotation = CGFloat(M_PI)
+        
+        _wallPair.addChild(topWall)
+        _wallPair.addChild(bottomWall)
+        _wallPair.addChild(scoreNode)
+        
+        _wallPair.zPosition = 1
+        
+        _wallPair.run(moveAndRemove)
+        
+        self.addChild(_wallPair)
     }
 }
